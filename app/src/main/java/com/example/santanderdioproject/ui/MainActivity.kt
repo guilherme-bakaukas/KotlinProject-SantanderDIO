ef503ee0a907833ef0879d7a5539f400c11b5d33
guilherme-bakaukas <guilherme.tezoli@hotmail.com>
2021-09-23 01:27:48 -0300

    delete implementation

--- app/src/main/java/com/example/santanderdioproject/ui/MainActivity.kt
+++ app/src/main/java/com/example/santanderdioproject/ui/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.santanderdioproject.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.santanderdioproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.nav_host_fragment))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
